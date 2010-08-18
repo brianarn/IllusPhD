f2050cdc7f8a404e3160bb0975b9660c908298f0
Brian Arnold <brianarn@gmail.com>
2010-08-17 22:02:17 -0600

    Saving apart the separate pieces for the BS

--- init.js
+++ init.js
@@ -7,11 +7,10 @@ dojo.require('dojox.gfx');
 dojo.ready(function(){
 	// Variables in here for cleaner scope
 	var container, guide, gwidth, gheight,
-		cx, cy, rAll, rElem, rHS, rBS,
-		bsOffset, rBSSpec,
-		colElem, colHS, colBS,
-		allKnown, elem, hs, bs,
-		phdPre, phdPost;
+		cx, cy,
+		allKnown, elem, hs, bsGrp, bsBase, bsSpec, phdPre, phdPost,
+		rAll, rElem, rHS, rBS, rBSSpec, offsetBSSpec,
+		colElem, colHS, colBS, colMS;
 
 	// Mmm, log
 	console.log('IllusPhD.init starting...');
@@ -32,13 +31,15 @@ dojo.ready(function(){
 	colHS = dojo.colorFromString('#97d065');
 	rBS = rAll * 0.25;
 	colBS = dojo.colorFromString('#f18a81');
-	bsOffset = rAll * 0.18;
 	rBSSpec = rAll * 0.08;
+	offsetBSSpec = rAll * 0.18;
+	colMS = dojo.colorFromString('#ef5f54');
 
 	// Draw the elements
-	bs = guide.createGroup();
-	bs.add(guide.createCircle({cx: cx, cy: cy, r: rBS}).setFill(colBS));
-	bs.add(guide.createCircle({cx: cx + bsOffset, cy: cy - bsOffset, r: rBSSpec}).setFill(colBS));
+	bsBase = guide.createCircle({cx: cx, cy: cy, r: rBS}).setFill(colBS);
+	bsSpec = guide.createCircle({cx: cx + offsetBSSpec, cy: cy - offsetBSSpec, r: rBSSpec}).setFill(colBS);
+	bsGrp = guide.createGroup();
+	bsGrp.add(bsBase).add(bsSpec);
 	hs = guide.createCircle({cx: cx, cy: cy, r: rHS}).setFill(colHS);
 	elem = guide.createCircle({cx: cx, cy: cy, r: rElem}).setFill(colElem);
 	allKnown = guide.createCircle({cx: cx, cy: cy, r: rAll}).setStroke({color: 'black', width: 2});
