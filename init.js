6558a487d932b920ea80b24917e46df9d5b8030b
Brian Arnold <brianarn@gmail.com>
2010-08-17 21:06:36 -0600

    Initial commit

---
+++ init.js
@@ -0,0 +1,7 @@
+// Start it up, woo woo
+dojo.provide("IllusPhD.init");
+
+dojo.ready(function(){
+	console.log('IllusPhD.init starting...');
+	dojo.byId("demoContainer").innerHTML="<h1>Hello World</h1>";
+});
---
+++ layer.js
@@ -0,0 +1,7 @@
+dojo.provide("IllusPhD.layer");
+
+// The path for this demo module
+dojo.registerModulePath("IllusPhD","../../IllusPhD");
+
+// Start up the demo
+dojo.require("IllusPhD.init");
